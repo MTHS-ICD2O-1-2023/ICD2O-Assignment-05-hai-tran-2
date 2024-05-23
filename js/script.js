04df95e27c9edf6c5b81436c764363628049e5d1
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-05-23 00:22:58 +0000

    add webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function findNTermOfFibonacciSequence() {
+  // input
+  const nTermNumber =
+    parseInt(document.getElementById("n-term-number").value) - 2
+  let answer = 0
+  let firstNumber = 0
+  let secondNumber = 1
+
+  if (nTermNumber < 0) {
+    document.getElementById("result").innerHTML = firstNumber
+  } else {
+    document.getElementById("result").innerHTML =
+      firstNumber + ", " + secondNumber + ", "
+    answer = firstNumber + secondNumber
+    for (let counter = 0; counter < nTermNumber; counter++) {
+      // output
+      answer = firstNumber + secondNumber
+      document.getElementById("result").innerHTML =
+        document.getElementById("result").innerHTML + answer + ", "
+      firstNumber = secondNumber
+      secondNumber = answer
+    }
+  }
+  document.getElementById("result").innerHTML =
+    document.getElementById("result").innerHTML +
+    "<br />" +
+    "The nth term is " +
+    answer
+}
